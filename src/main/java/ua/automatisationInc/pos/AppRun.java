38dd8b8f6c6ebb3809325bbabe47789a56feec68
plgrudina <plgrudina@gmail.com>
2018-03-24 20:42:35 +0200

    dependency add

---
+++ src/main/java/ua/automatisationInc/pos/AppRun.java
@@ -0,0 +1,19 @@
+package ua.automatisationInc.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Pavel Grudina on 24.03.2018.
+ */
+
+@SpringBootApplication
+public class AppRun {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(AppRun.class, args);
+
+    }
+}
+
