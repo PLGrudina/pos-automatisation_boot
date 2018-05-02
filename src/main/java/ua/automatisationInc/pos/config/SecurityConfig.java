493be4981025cdf229dfa19af951fb3a38bac38d
plgrudina <plgrudina@gmail.com>
2018-05-02 23:59:05 +0300

    security add

--- src/main/java/ua/automatisationInc/pos/config/SecurityConfig.java
+++ src/main/java/ua/automatisationInc/pos/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/index", "/public/*").permitAll()
+                .antMatchers("/public/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
